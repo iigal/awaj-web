f7d55f8b115f80674fd2fcc1417915f95373a836
Shashank Shree Neupane <shashank@neupane.com.np>
2024-08-22 23:56:04 +0545

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-// mongoose.connect(
-//   "mongodb+srv://complaintmgmt1366:GLz2blyKA3RttsWx@cluster0.lcpvryx.mongodb.net/?retryWrites=true&w=majority"
-// );
+
 mongoose.connect(
     process.env.DB_URL
 );
